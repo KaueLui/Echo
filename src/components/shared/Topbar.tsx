46df38ef40d47e12213120efd1f27f297c5df8db
Kauê Luí <ofckaue@gmail.com>
2023-12-26 15:03:20 -0300

    Setting up homepage and putting sidebar routes to work.

--- src/components/shared/Topbar.tsx
+++ src/components/shared/Topbar.tsx
@@ -1,13 +1,34 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom';
+import { Button } from '../ui/button';
+import { useSignOutAccount } from '@/lib/react-query/queriesAndMutations';
+import { useEffect } from 'react';
+import { useUserContext } from '@/context/AuthContext';
 
 const Topbar = () => {
+  const { mutate: signOut, isSuccess } = useSignOutAccount();
+  const navigate = useNavigate();
+  const { user } = useUserContext();
+
+  useEffect(() => {
+    if (isSuccess) navigate(0);
+  }, [isSuccess])
+  
   return (
     <section className='topbar'>
       <div className='flex-between py-4 px-5'>
         <Link to="/" className="flex gap-3 items-center">
-        <img src="/assets/images/logo-2.svg" alt="logo" />
+        <img src="/assets/images/logo-2.svg" alt="logo" width={130} height={325} />
         </Link>
-        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">Create a new account</h2>
+
+        <div className='flex gap-4'>
+          <Button variant="ghost" className="shad-button_ghost" onClick={() => signOut()}>
+          <img src="/assets/icons/logout.svg" alt="logout" />
+          </Button>
+          <Link to={`/profile/${user.id}`} className='flex-center gap-3'>
+            <img src={user.imageUrl || '/assets/images/profile.png'} alt="profile" className='h-8 w-8 rounded-full'/>
+
+          </Link>
+        </div>
       </div>
 
     </section>
--- src/lib/react-query/queriesAndMutations.tsx
+++ src/lib/react-query/queriesAndMutations.tsx
@@ -4,7 +4,7 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createUserAccount, signInAccount } from '../appwrite/api'
+import { createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewUser } from '@/types'
 
 export const useCreateUserAccount = () => {
@@ -19,5 +19,10 @@ export const useSignAccount = () => {
             email: string; 
             password: string;
         }) => signInAccount(user),
+    });
+}
+ export const useSignOutAccount = () => {
+    return useMutation({
+        mutationFn: signOutAccount
     });
 }
