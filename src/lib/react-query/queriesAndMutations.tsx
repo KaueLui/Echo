709cc5e3578aedfede9780b9dab3baa4b1f5208f
Kauê Luí <ofckaue@gmail.com>
2023-12-27 16:35:09 -0300

    Home page is working

--- src/lib/react-query/queriesAndMutations.tsx
+++ src/lib/react-query/queriesAndMutations.tsx
@@ -4,8 +4,9 @@ import {
     useQueryClient,
     useInfiniteQuery,
 } from '@tanstack/react-query'
-import { createPost, createUserAccount, signInAccount, signOutAccount } from '../appwrite/api'
+import { createPost, createUserAccount, getRecentPosts, signInAccount, signOutAccount } from '../appwrite/api'
 import { INewPost, INewUser } from '@/types'
+import { QUERY_KEYS } from './queryKeys';
 
 export const useCreateUserAccount = () => {
     return useMutation({
@@ -28,7 +29,20 @@ export const useSignAccount = () => {
 }
 
 export const useCreatePost = () => {
+    const queryClient = useQueryClient();
     return useMutation({
-        mutationFn: (post: INewPost) => createPost(post)
+        mutationFn: (post: INewPost) => createPost(post),
+        onSuccess: () => {
+            queryClient.invalidateQueries({ 
+                queryKey: [QUERY_KEYS.GET_RECENT_POSTS] 
+            })
+        }
+    });
+}
+
+export const useGetRecentPosts = () => {
+    return useQuery({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+        queryFn: getRecentPosts
     });
 }
